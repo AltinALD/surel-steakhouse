fbbe40e320b9f5ff3c3c76af2727e0dde026d58f
AltinALD <ae30690@seeu.edu.mk>
2026-03-04 14:01:55 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+  const nav = document.getElementById("nav");
+  nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+}
+
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}, 4000);
